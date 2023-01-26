039d0e881db711e427f8ca8f2d925d72e4cfe962
mariaalexandra12 <mocanumariaalexandra53@gmail.com>
2023-01-26 22:34:07 +0200

    add

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 var number=5;
-var name='gigi';
-console.log(name);
+var name="gigi";
+console.log(number);
+
+var array=new Array();
+array.push(15);
+array.push(12);
+array.push(20);
+array.push(25);
+//array.pop(25);
+//array.shift();
+array.unshift(67);
+console.log(array);
+
+var shoppingList=[["cereal",3],
+["milk",25],
+["apple",52],
+["bananas",63]];
+console.log(shoppingList);
+
+function reusableFunction(){
+    console.log("Salut");
+}
+
+reusableFunction();
+
+
+function salut(){
+    console.log("Buna ziua");
+}
+
+salut();
+
+function arg(a,b){
+    console.log(a+b);
+}
+arg(2,5);
+arg("Buna ziua",",lume!");
+
+
+
+
+
+
+
+
+
+
+
+
